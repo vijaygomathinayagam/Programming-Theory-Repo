a47be007d6d0ed59493fccbff59083046cd7295d
Vijay Gomathinayagam <31568585+vijaygomathinayagam@users.noreply.github.com>
2025-07-02 15:43:22 +0530

    Add quit button
    implement quit button, on clicking it the game should be exited

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,6 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,5 +9,14 @@ public class MenuManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(Constants.GameSceneOrder);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
